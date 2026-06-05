1cfcc632cd05dc12a0f8f34cf51146c5ff33b72e
Austin <joshcamacho413@gmail.com>
2026-06-05 14:15:59 +0800

    ci: next

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push("@sparticuz/chromium");
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
