b743639ad446a1cad67fd9c2a8a836db577d8dc8
Austin <joshcamacho413@gmail.com>
2026-06-19 10:10:13 +0800

    resumely logo size

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -15,8 +15,8 @@ const Navbar = ({ rightSlot }: NavbarProps) => {
         <Image
           src="/resumely.png"
           alt="logo"
-          width={120}
-          height={120}
+          width={100}
+          height={100}
         />
       </Link>
 
